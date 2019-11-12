a81c1e472c9059d394b563a801653cf496334e04
ptabasso2 <pejman.tabassomi@gmail.com>
2019-11-12 10:55:28 +0100

    Deleting log directory

---
+++ src/main/java/datadog/opentracing/scopemanager/ContextualScopeManager.java
@@ -0,0 +1,63 @@
+package datadog.opentracing.scopemanager;
+
+import datadog.opentracing.DDSpan;
+import datadog.trace.context.ScopeListener;
+import io.opentracing.Scope;
+import io.opentracing.ScopeManager;
+import io.opentracing.Span;
+
+import java.util.Deque;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ContextualScopeManager implements ScopeManager {
+  static final ThreadLocal<Scope> tlsScope = new ThreadLocal<>();
+  final Deque<ScopeContext> scopeContexts = new ConcurrentLinkedDeque<>();
+  final List<ScopeListener> scopeListeners = new CopyOnWriteArrayList<>();
+
+  @Override
+  public Scope activate(final Span span, final boolean finishOnClose) {
+    for (final ScopeContext context : scopeContexts) {
+      if (context.inContext()) {
+        return context.activate(span, finishOnClose);
+      }
+    }
+    if (span instanceof DDSpan) {
+      return new ContinuableScope(this, (DDSpan) span, finishOnClose);
+    } else {
+      return new SimpleScope(this, span, finishOnClose);
+    }
+  }
+
+  @Override
+  public Scope activate(final Span span) {
+    return activate(span, false);
+  }
+
+  @Override
+  public Scope active() {
+    for (final ScopeContext csm : scopeContexts) {
+      if (csm.inContext()) {
+        return csm.active();
+      }
+    }
+    return tlsScope.get();
+  }
+
+  @Override
+  public Span activeSpan() {
+    final Scope active = active();
+    return active == null ? null : active.span();
+  }
+
+  @Deprecated
+  public void addScopeContext(final ScopeContext context) {
+    scopeContexts.addFirst(context);
+  }
+
+  /** Attach a listener to scope activation events */
+  public void addScopeListener(final ScopeListener listener) {
+    scopeListeners.add(listener);
+  }
+}
---
+++ src/main/java/datadog/trace/common/writer/LoggingWriter.java
@@ -0,0 +1,43 @@
+package datadog.trace.common.writer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import datadog.opentracing.DDSpan;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class LoggingWriter implements Writer {
+  private final ObjectMapper serializer = new ObjectMapper();
+
+  @Override
+  public void write(final List<DDSpan> trace) {
+    if (log.isInfoEnabled()) {
+      try {
+        log.info("write(trace): {}", serializer.writeValueAsString(trace));
+      } catch (final Exception e) {
+        log.error("error writing(trace): {}", trace);
+      }
+    }
+  }
+
+  @Override
+  public void incrementTraceCount() {
+    log.info("incrementTraceCount()");
+  }
+
+  @Override
+  public void close() {
+    log.info("close()");
+  }
+
+  @Override
+  public void start() {
+    log.info("start()");
+  }
+
+  @Override
+  public String toString() {
+    return "LoggingWriter { }";
+  }
+}
